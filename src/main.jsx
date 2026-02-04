290050b92a5df33276849351a62c4672e978f99c
Lipécz Detre Platón <51795664+D3TR3@users.noreply.github.com>
2026-02-04 10:19:17 +0100

    Added Routes for the home page and the countries

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from './pages/home-page/HomePage.jsx'
+import Countries from './pages/countries/Countries.jsx'
+import Country from './pages/countries/country/Country.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route index path='/home' element={<HomePage />} />
+    </Routes>
+    <Route>
+      <Route index path='/countries' element={<Countries />} />
+      <Route path=':countrycode' element={<Country />} />
+    </Route>    
+  </BrowserRouter>,
 )
